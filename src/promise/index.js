804b654355f355538dcbba07bb2f677b70a3d392
Alexis Liao <Alexisliao10@hotmail.com>
2023-11-23 13:21:47 -0500

    Promise added

--- src/promise/index.js
+++ src/promise/index.js
@@ -1,14 +1,14 @@
-const promise = new Promise((resolve, reject) => {
-  resolve("Hey!");
+const promise = new Promise(function (resolve, reject) {
+  resolve("hey!");
 });
 
 const cows = 15;
 
-const countCows = new Promise((resolve, reject) => {
+const countCows = new Promise(function (resolve, reject) {
   if (cows > 10) {
     resolve(`We have ${cows} cows on the farm`);
   } else {
-    reject("There is no sufficient cows in the farm");
+    reject("There is no cows on the farm");
   }
 });
 
